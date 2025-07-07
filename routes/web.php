76df64bf49fa9de3d0913d8a7834f41e2ce5183f
gianca04 <grancarlosavalogallardo@gmail.com>
2025-07-06 21:46:25 -0500

    Ajuste de fomrularios y resouces

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Livewire\Livewire;
+
+Livewire::setScriptRoute(function ($handle) {
+return Route::get('/monitor/public/livewire/livewire.js', $handle);
+});
+
+Livewire::setUpdateRoute(function ($handle) {
+return Route::post('/monitor/public/livewire/update', $handle);
+});
